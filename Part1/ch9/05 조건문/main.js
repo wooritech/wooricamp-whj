a9de779fc0319c9b5733f4a25ae130080bda45a4
whj <whj9910@gmail.com>
2023-12-28 08:58:05 +0900

    part1

--- Part1/ch9/05 조건문/main.js
+++ Part1/ch9/05 조건문/main.js
@@ -1,5 +1,5 @@
 let isShow = true;
-let cheked = false;
+let checked = false;
 
 if(isShow) {
   console.log('Show!'); // Show!
@@ -12,7 +12,7 @@ if (checked) {
 let coconet = true;
 
 if (coconet) {
-  console.log('Show!');
+  console.log('Show!'); //Show!
 } else {
-  console.log ('why?');
+  console.log ('why?'); //만약 coconet이 false라면 Show!는 출력 되지 않고 why?출력
 }
