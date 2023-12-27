7cffc3def0d25b2a09e07d37feac74fbf2c8e70c
whj <whj9910@gmail.com>
2023-12-27 17:59:24 +0900

    part1

---
+++ Part1/ch9/05 조건문/main.js
@@ -0,0 +1,18 @@
+let isShow = true;
+let cheked = false;
+
+if(isShow) {
+  console.log('Show!'); // Show!
+}
+
+if (checked) {
+  console.log('Checked!');
+}
+
+let coconet = true;
+
+if (coconet) {
+  console.log('Show!');
+} else {
+  console.log ('why?');
+}
