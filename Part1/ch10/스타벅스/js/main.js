33621933782624adf73a658ad491003815aaf244
whj <whj9910@gmail.com>
2023-12-29 15:24:08 +0900

    part1

---
+++ Part1/ch10/스타벅스/js/main.js
@@ -0,0 +1,49 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function(){
+  // Logic..
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
+});
+
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간(ms)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+   // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
---
+++ Part1/ch10/스타벅스/readme.md
@@ -0,0 +1,14 @@
+# 문자 포트 변경
+
+- google font
+
+# 구글 제공 아이콘
+
+- google Material icons
+
+# BEM
+
+- Block Element Modifier
+- HTML 클래스 속성의 작명법
+  - 요소__일부분 -> 요소의 일부분을 표시
+  - 요소--상태 -> 요소의 상태를 표시
