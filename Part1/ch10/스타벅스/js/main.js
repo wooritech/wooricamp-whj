abc9a6723b0d2a07c2bf1f144280eb48a1c87715
whj <whj9910@gmail.com>
2024-01-02 17:58:29 +0900

    part1

--- Part1/ch10/스타벅스/js/main.js
+++ Part1/ch10/스타벅스/js/main.js
@@ -47,3 +47,46 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 });
+
+
+// new Swiper (선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  // autoplay: true,
+  loop: true
+});
+
+ new Swiper('.promotion .swiper-container', {
+  // direction의 기본값 horizontal
+  direction: 'horizontal',
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 3000
+  // },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 제어
+    clickable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl:'.promotion .swiper-prev',
+    nextEl:'.promotion .swiper-next'
+  }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    //숨김처리
+    promotionEl.classList.add('hide');
+  }else {
+    //보임처리
+    promotionEl.classList.remove('hide');
+  }
+});
