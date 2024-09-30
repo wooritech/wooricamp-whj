151dfe04980516995d7383a61c9e3c1c00357e5b
Hjname <whj9910@gmail.com>
2024-09-30 10:48:34 +0900

    prettierignore 기능 설정 (코드 작성후 >format으로 적용되게 만들었다.)

--- back_joon/1406/1406.js
+++ back_joon/1406/1406.js
@@ -1,18 +1,18 @@
-const fs = require("fs")
-const input =  fs.readFileSync("/dev/stdin").toString().trim().split("\n")
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let Lstack = input[0].split('');
-let Rstack = [];
-for(let i=2;i<input.length;i++){
-    let array = input[i].split(" ");
-    if(array[0]==='L'&&Lstack.length){ 
-        Rstack.push(Lstack.pop());
-    }else if(array[0]==="D"&&Rstack.length){   
-        Lstack.push(Rstack.pop());
-    }else if(array[0]==="B"){   
-        Lstack.pop();
-    }else if(array[0]==="P"){
-        Lstack.push(array[1]);
+let Lstack = input[0].split('')
+let Rstack = []
+for (let i = 2; i < input.length; i++) {
+    let array = input[i].split(' ')
+    if (array[0] === 'L' && Lstack.length) {
+        Rstack.push(Lstack.pop())
+    } else if (array[0] === 'D' && Rstack.length) {
+        Lstack.push(Rstack.pop())
+    } else if (array[0] === 'B') {
+        Lstack.pop()
+    } else if (array[0] === 'P') {
+        Lstack.push(array[1])
     }
 }
-console.log(Lstack.concat(Rstack.reverse()).join(''));
+console.log(Lstack.concat(Rstack.reverse()).join(''))
