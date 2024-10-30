ff89e19529f6d15856ac642b9319b27cb5d414ae
Hjname <whj9910@gmail.com>
2024-10-30 10:42:00 +0900

    코드 해설 주석 추가

--- back_joon/boj2/boj2.spec.js
+++ back_joon/boj2/boj2.spec.js
@@ -4,21 +4,21 @@ const testCases1 = [
     [3, 4],
     [9, 8],
     [5, 2],
-		[9, 9], // 조건 내 최대값
-		[-1, -5], // 음수
-		[100, 999] // 조건 초과
+    [9, 9], // 조건 내 최대값
+    [-1, -5], // 음수
+    [100, 999], // 조건 초과
 ];
 
 const testCases2 = [
-		'8',
+    '8',
     'I am happy today',
     'We want to win the first prize',
     'Nursery rhyme and Mother Goose', // 대문자 혼용하기
     'What are little boys made of?', // 특수 기호 사용하기
     'Frogs and snails',
     'And puppys tails',
-		'That is what little boys are made of',
-		'qwertyuiopasdfghjklzxcvbnm' // 단어 길이 제한 초과
+    'That is what little boys are made of',
+    'qwertyuiopasdfghjklzxcvbnm', // 단어 길이 제한 초과
 ];
 
 module.exports = {
